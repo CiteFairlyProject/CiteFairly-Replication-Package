19955c91b2bd25ca0b44354b26cfd91dfeb34114
amanuelak <amanuel9@my.yorku.ca>
2026-05-16 17:05:29 -0400

    added the loop that calculates the cosine similarity scores

--- RQ4_Results/Code/cosine_similarity.py
+++ RQ4_Results/Code/cosine_similarity.py
@@ -16,7 +16,7 @@ BERT_model.eval()
 
 
 
-main_dir = "" # set your working directory
+main_dir = "" # set your working directory that contains the json files for ESEM, EASE, and SANER
 output_dir = os.path.join(main_dir, "cosine_scores.csv")
 plot_png = os.path.join(main_dir, "cosine_score_dist.png")
 conf_list = ["ESEM", "EASE", "SANER"]
@@ -39,7 +39,32 @@ def concat_title_abstract(paper_json):
     return f"{title} {abstract}"
 
 
+cosine_scores = []
 
 
+for conf in conf_list:
+    conf_dir = os.path.join(main_dir, conf) # direcotry for each conferences in working directory
+    json_files = os.listdir(conf_dir)
 
+    for json_file in json_files:
+        with open(os.path.join(conf_dir, json_file), "r") as f:
+            paper_json = json.load(f) #loads the json file for each paper in the conference
+        
+        paper_vector = convert_to_vector(concat_title_abstract(paper_json)) # converts the title and abstract to vector using BERT
+
+
+        recommendation = paper_json.get("related", []) 
+
+        for rec in recommendation:
+            recommendation_vector = convert_to_vector(concat_title_abstract(rec)) # converts title and abstract of each recommended paper to vector
+
+            cosine_score = cosine_similarity(paper_vector, recommendation_vector)[0][0] # calculates the cosine score
+
+            cosine_scores.append({
+                "conference":   conf,
+                "year":         paper_json.get("year", ""),
+                "paper":        paper_json.get("title", ""),
+                "rec_title":    rec.get("title", ""),
+                "cosine_score": float(cosine_score),
+            })
 
