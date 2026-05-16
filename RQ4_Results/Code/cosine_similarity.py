22bc71d3050eef23c75472d18f234c22baa15399
amanuelak <amanuel9@my.yorku.ca>
2026-05-16 16:33:36 -0400

    added the function that concatentates the title and abstract

--- RQ4_Results/Code/cosine_similarity.py
+++ RQ4_Results/Code/cosine_similarity.py
@@ -23,11 +23,23 @@ conf_list = ["ESEM", "EASE", "SANER"]
 
 
 
-# Convert title and abstract to vector using BERT
+# Converts title and abstract to vector using BERT
 def convert_to_vector(title_and_abstract):
     tokenized_text = BERT_tokenizer(title_and_abstract, return_tensors="pt", truncation=True, padding=True).to(device)
     with torch.no_grad():
         bert_output = BERT_model(**tokenized_text)
     
     return bert_output.last_hidden_state[:, 0, :].cpu().numpy() #
+
+
+# concatenates the title and abstract
+def concat_title_abstract(paper_json):
+    title = paper_json.get("title", "")
+    abstract = paper_json.get("abstract", "")
+    return f"{title} {abstract}"
+
+
+
+
+
 
