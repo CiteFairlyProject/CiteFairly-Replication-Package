16b0e726311a2eff10ff7d16627b27fb3e59a0f0
amanuelak <amanuel9@my.yorku.ca>
2026-05-16 16:28:32 -0400

    added script to load model and convert title and abtract to vector

---
+++ RQ4_Results/Code/cosine_similarity.py
@@ -0,0 +1,33 @@
+import json
+import os
+import torch
+import pandas as pd
+import matplotlib.pyplot as plt
+from transformers import BertTokenizer, BertModel
+from sklearn.metrics.pairwise import cosine_similarity
+
+
+
+# Load the BERT model and tokenizer
+device    = torch.device("cuda" if torch.cuda.is_available() else "cpu") 
+BERT_tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
+BERT_model     = BertModel.from_pretrained("bert-base-uncased").to(device)
+BERT_model.eval()
+
+
+
+main_dir = "" # set your working directory
+output_dir = os.path.join(main_dir, "cosine_scores.csv")
+plot_png = os.path.join(main_dir, "cosine_score_dist.png")
+conf_list = ["ESEM", "EASE", "SANER"]
+
+
+
+# Convert title and abstract to vector using BERT
+def convert_to_vector(title_and_abstract):
+    tokenized_text = BERT_tokenizer(title_and_abstract, return_tensors="pt", truncation=True, padding=True).to(device)
+    with torch.no_grad():
+        bert_output = BERT_model(**tokenized_text)
+    
+    return bert_output.last_hidden_state[:, 0, :].cpu().numpy() #
+
